399a1601495012ed36a2fe6b6db360044381258c
Maciej Ciach <maciej.ciach@knowde.com>
2024-11-08 17:17:14 -0600

    DEVOPS: Update octane version

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM ghcr.io/ai-dock/linux-desktop:latest
 
-COPY OctaneRender_Studio+_2022_1_1_linux.zip /
+COPY OctaneRender_Studio+_2024_1_linux.zip /
 
-# RUN mkdir -p /workspace/home/user/Desktop && unzip /workspace/home/user/OctaneRender_Studio+_2022_1_1_linux.zip -d /workspace/home/user/Desktop
+# RUN mkdir -p /workspace/home/user/Desktop && unzip /workspace/home/user/OctaneRender_Studio+_2024_1_linux.zip -d /workspace/home/user/Desktop
 
 # RUN sudo mkdir /etc/OctaneRender
 # COPY otoy_unattended_credentials /etc/OctaneRender/
@@ -13,7 +13,7 @@ RUN curl -L -o /cudnn_8_0_4_linux.zip https://render.otoy.com/downloads/66/0c/c6
 
 # RUN cp /cudnn_8_0_4_linux.zip /opt/storage/
 
-# COPY OctaneRender_Studio+_2022_1_1_linux.zip /opt/storage/OctaneRender_Studio+_2022_1_1_linux.zip
+# COPY OctaneRender_Studio+_2024_1_linux.zip /opt/storage/OctaneRender_Studio+_2024_1_linux.zip
 
 COPY provisioning.sh /opt/ai-dock/bin/provisioning.sh
 
@@ -21,4 +21,4 @@ COPY provisioning.sh /opt/ai-dock/bin/provisioning.sh
 
 # RUN rm /workspace/home/user/.OctaneRender/thirdparty/cudnn_8_0_4/cudnn_8_0_4_linux.zip
 
-# RUN rm /workspace/home/user/OctaneRender_Studio+_2022_1_1_linux.zip
+# RUN rm /workspace/home/user/OctaneRender_Studio+_2024_1_linux.zip
--- provisioning.sh
+++ provisioning.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-unzip /OctaneRender_Studio+_2022_1_1_linux.zip -d /home/user/Desktop/ 
+unzip /OctaneRender_Studio+_2024_1_linux.zip -d /home/user/Desktop/ 
 mkdir -p /workspace/home/user/.OctaneRender/thirdparty/cudnn_8_0_4
 unzip /cudnn_8_0_4_linux.zip -d /workspace/home/user/.OctaneRender/thirdparty/cudnn_8_0_4/
-curl -o /home/user/Desktop/OctaneRender_Studio+_2022_1_1_linux/script.lua -L "https://render.otoy.com/forum/download/file.php?id=85243&sid=54080bed235fc68b437cfd3c13c164d2"
-chown -R user:user /home/user/Desktop/OctaneRender_Studio+_2022_1_1_linux/
+curl -o /home/user/Desktop/OctaneRender_Studio+_2024_1_linux/script.lua -L "https://render.otoy.com/forum/download/file.php?id=85243&sid=54080bed235fc68b437cfd3c13c164d2"
+chown -R user:user /home/user/Desktop/OctaneRender_Studio+_2024_1_linux/
 sudo -u user bash -c 'export HOME=/home/user && curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install'
 sudo -u user bash -c 'export HOME=/home/user && mkdir ~/Desktop/kaczka'
 sudo -u user echo "s3://${S3_BUCKET}/TU_ZMIEN_SOBIE/" > /home/user/Desktop/kaczka/lokalizacja.txt
@@ -24,7 +24,7 @@ do
 done ' > /home/user/Desktop/kaczka/upload.sh
 sudo -u user echo '#!/bin/bash
 find . -name "*.orbx"|while read fname; do
-/home/user/Desktop/OctaneRender_Studio+_2022_1_1_linux/octane "$fname" --script /home/user/Desktop/OctaneRender_Studio+_2022_1_1_linux/script.lua --no-gui
+/home/user/Desktop/OctaneRender_Studio+_2024_1_linux/octane "$fname" --script /home/user/Desktop/OctaneRender_Studio+_2024_1_linux/script.lua --no-gui
 done' > /home/user/Desktop/kaczka/octane_start.sh
 chown -R user:user /home/user/Desktop/kaczka/
 sudo -u user bash -c "chmod +x /home/user/Desktop/kaczka/upload.sh"
